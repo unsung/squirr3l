9f62e000e6f9531ae9cf892f73887293a68b6bb9
Josh Manning <unsungmaster@gmail.com>
2014-05-01 13:31:40 -0700

    broken probe

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ int main() {
 
 	double r0, t0, v0;
 	double dt, t, tf;
-	double a;
 
 	r0 = 1;
 	t0 = 1; // t0 = r0*sqrt(r0/GME);//hr
@@ -15,21 +14,18 @@ int main() {
 	dt=1.e-4;
 
 	State sun     = {0, 0, 0  ,0, 1};
-	State earth   = {1, 0, 0  ,1, 3e-6};
-
-	a=M_PI/180*60;
-	State jupiter = {5*cos(a), 5*sin(a),-.5*sin(a),.5*cos(a), 9.5511e-4};
+	//State earth   = {1, 0, 0  ,1, 3e-6};
+	State earth   = {1, 0, 0  ,1, 0};
+	State jupiter = {0, 5,-.5,0, 9.5511e-4};
 
 	State probe   = earth;
 
-	probe.vx += 0.2;
-	probe.vy += 0.2;
+	probe.vy += 0.01;
 
-	State* planet_bodies[] = {&sun};
-	State* probe_bodies[] = {&sun, &earth, &jupiter};
+	State* probe_bodies[] = {&sun, &jupiter};
 
 	for(t=0;t<2*M_PI;t+=dt) {
-		update(dt, &probe  , probe_bodies  , 3);
+		update(dt, &probe  , probe_bodies  , 2);
 		update_planet(dt, &earth);
 		update_planet(dt, &jupiter);
 
@@ -38,7 +34,6 @@ int main() {
 				earth.x, earth.y,
 				jupiter.x, jupiter.y);
 	}
-	//printf("%f\n"
 
 	return 0;
 
